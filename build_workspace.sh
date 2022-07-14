372398d12bf3411b3b41f5076a3a68859a844f82
Serafadam <serafadam@gmail.com>
2022-07-14 18:30:34 +0200

    fix build script

--- build_workspace.sh
+++ build_workspace.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-colcon build --symlink-install --cmake-args '-DCMAKE_BUILD_TYPE=RelWithDebInfo' '-DCMAKE_EXPORT_COMPILE_COMMANDS=On' '-DCMAKE_POSITION_INDEPENDENT_CODE=On' '-DBUILD_SHARED_LIBS=On' -Wall -Wextra -Wpedantic
+colcon build --symlink-install  --cmake-args -DCMAKE_BUILD_TYPE=Release --cmake-args -DBUILD_TESTING=OFF --cmake-args -DCMAKE_EXPORT_COMPILE_COMMANDS=ON --cmake-args -DCMAKE_POSITION_INDEPENDENT_CODE=ON --cmake-args -DBUILD_SHARED_LIBS=ON
